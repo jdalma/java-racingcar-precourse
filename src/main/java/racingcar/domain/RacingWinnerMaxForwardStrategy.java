270eca90a5859e4c9890f6573a5c1a0da537d474
정현준 <jeongdalma@gmail.com>
2022-10-10 15:14:46 +0900

    fix(RacingWinnerMaxForwardStrategy) : 우승자 선택 전략 StringJoiner로 수정

--- src/main/java/racingcar/domain/RacingWinnerMaxForwardStrategy.java
+++ src/main/java/racingcar/domain/RacingWinnerMaxForwardStrategy.java
@@ -1,14 +1,15 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class RacingWinnerMaxForwardStrategy implements WinnerCondition {
 
     @Override
     public String getWinners(List<Car> cars, int maxForwardCount) {
-        StringBuilder result = new StringBuilder();
+        StringJoiner result = new StringJoiner("\n");
         for (Car car : cars) {
-            result.append(car.toStringIfWinner(maxForwardCount)).append("\n");
+            result.add(car.toStringIfWinner(maxForwardCount));
         }
         return result.toString();
     }
